d9b6dab43b513756bafe2729a8204af60adb3700
Lunusushi <luis.nunez.11037@gmail.com>
2025-06-19 17:21:24 -0400

    Sistema - BackEnd
    Se produjeron los siguientes cambios:
    - Se iniciaron los models.

---
+++ Sistema/Backend/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/Cobres_Desa'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
--- Sistema/Backend/models/usuario.py
+++ Sistema/Backend/models/usuario.py
@@ -1,1 +1,9 @@
-print("Hola mundo")
+from database import db
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100))
+    apellido = db.Column()
+    pass
