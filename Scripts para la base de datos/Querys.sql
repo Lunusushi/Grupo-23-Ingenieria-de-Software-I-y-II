83998e697ac260ace565be0e2e4032564a9565ac
Lunusushi <luis.nunez.11037@gmail.com>
2025-06-20 18:06:09 -0400

    Final Incremento 1

--- Scripts para la base de datos/Querys.sql
+++ Scripts para la base de datos/Querys.sql
@@ -5,9 +5,9 @@ select * from producto;
 select * from pedido;
 
 delete from operador where id_usuario = 1;
-delete from cliente where id_usuario = 5;
+delete from cliente where id_usuario = 3;
 
 insert into operador (id_operador, id_usuario, cargo)
-values(1, 2, "administrador");
+values(2, 3, "administrador");
 
 delete from producto where id_producto = 2;
