e0791f3f2f41244b3e862425e6349bce627b5d1b
Lunusushi <luis.nunez.11037@gmail.com>
2025-11-04 23:23:16 -0300

    Update RegistroProductos.sql

--- Scripts para la base de datos/RegistroProductos.sql
+++ Scripts para la base de datos/RegistroProductos.sql
@@ -9,8 +9,8 @@ VALUES
 -- Tabla PRODUCTO
 INSERT INTO PRODUCTO (id_producto, nombre_producto, descripcion, precio_unitario, stock_actual, url_imagen_principal, activo, id_categoria)
 VALUES 
-(1, 'Biblia Católica Edición Pastoral', 'Biblia con tapa dura, tamaño mediano', 12000.00, 20, 'public/css/img/rosario_de_madera.jpeg', TRUE, 1),
-(2, 'Rosario de Madera', 'Rosario tradicional hecho a mano con cuentas de madera', 2500.00, 50, 'public/css/img/biblia.jpeg', TRUE, 2);
+(1, 'Biblia Católica Edición Pastoral', 'Biblia con tapa dura, tamaño mediano', 12000.00, 20, 'public/css/img/biblia.jpeg', TRUE, 1),
+(2, 'Rosario de Madera', 'Rosario tradicional hecho a mano con cuentas de madera', 2500.00, 50, 'public/css/img/rosario_de_madera.jpeg', TRUE, 2);
 
 
 -- Tabla IMAGEN_PRODUCTO
